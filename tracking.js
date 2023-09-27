f2674f3dd10e6c3e5f914c4cd1e2590ecdd22fbf
flytechAI <146145518+flytechAI@users.noreply.github.com>
2023-09-26 21:04:10 -0400

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -6,14 +6,48 @@ script.onload = function() {
 };
 document.head.appendChild(script);
 
-// Function to check if UTM paramet// The rest of your existing code...
+// Function to check if a specific UTM parameter exists with the given value in the URL
+function checkUtmParam(utmValue) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('utm_campaign') === utmValue;
+}
 
+// Function to get the user's IP address
+async function getUserIP() {
+    try {
+        let response = await fetch('https://api64.ipify.org?format=json');
+        let data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error('Error fetching IP:', error);
+    }
+}
+
+// Function to send data to Google Apps Script
+async function sendDataToSheet(ip, googleScriptURL) {
+    try {
+        await fetch(googleScriptURL, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ip: ip })
+        });
+        console.log("Data sent successfully");
+    } catch (error) {
+        console.error("Error sending data:", error);
+    }
+}
+
+// Fetch the specific client's configuration
 async function fetchConfig(clientId) {
     let configURL = `https://cdn.jsdelivr.net/gh/flytechAI/tracking-script@main/configs/${clientId}.json`;
     let response = await fetch(configURL);
     return await response.json();
 }
 
+// Main function to run on page load
 async function main() {
     // Fetch client ID from script tag
     let scripts = document.getElementsByTagName('script');
@@ -43,5 +77,4 @@ async function main() {
     }
 }
 
-// Call the main function on page load
-main();
+// Call the main function once the jQuery library is loaded
