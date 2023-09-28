3cc35d8dc6c990db38e48da592965e746c5aec5b
flytechAI <146145518+flytechAI@users.noreply.github.com>
2023-09-27 20:42:00 -0400

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -4,33 +4,28 @@ console.log("Script Loaded");
 const script = document.createElement('script');
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 script.onload = function() {
-    console.log("jQuery loaded. Calling main function...");
     main();
 };
 document.head.appendChild(script);
 
 // Function to check if UTM paramet
 function checkUtmParam(utmValue) {
-    console.log("Function checkUtmParam called");
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('utm_campaign') === utmValue;
 }
 
 // Function to get user's IP address
 async function getUserIP() {
-    console.log("Function getUserIP called");
     try {
         let response = await fetch('https://api64.ipify.org?format=json');
         let data = await response.json();
         return data.ip;
     } catch (error) {
-        console.error('Error fetching IP:', error);
     }
 }
 
 // Function to send data to Google Apps Script
 async function sendDataToSheet(ip, googleScriptURL) {
-    console.log("Function sendDataToSheet called");
     try {
         await fetch(googleScriptURL, {
             method: 'POST',
@@ -40,14 +35,12 @@ async function sendDataToSheet(ip, googleScriptURL) {
             },
             body: JSON.stringify({ ip: ip })
         });
-        console.log("Data sent successfully");
     } catch (error) {
         console.error("Error sending data:", error);
     }
 }
 
 async function fetchConfig(clientId) {
-    console.log("Function fetchConfig called");
     
     let configURL = `https://cdn.jsdelivr.net/gh/flytechAI/tracking-script@main/configs/6M6M6ZrR.json`;
     let response = await fetch(configURL);
@@ -56,8 +49,6 @@ async function fetchConfig(clientId) {
 
 async function main() {
     // Fetch client ID from script tag
-    console.log('All script tags:', document.getElementsByTagName('script'));
-    console.log('Filtered script tags:', [...document.getElementsByTagName('script')].filter(script => script.src.includes('tracking.js')));
 
     let scripts = document.getElementsByTagName('script');
     let currentScript = [...scripts].filter(script => script.src.includes('tracking.js'))[0];
@@ -92,9 +83,8 @@ async function main() {
     if (userIP) {
         sendDataToSheet(userIP, config.googleScriptURL);
     } else {
-        console.error("Could not retrieve user IP.");
+        console.error("Could not retrieve user ID.");
     }
 }
 
-console.log("Calling main function...");
 main();
